3bacc6d5952191de225df66845523c50788d72c1
trietng <93473943+trietng@users.noreply.github.com>
2022-12-17 14:20:08 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+﻿#include "sort.hpp"
+#include "output.hpp"
+#include "file.hpp"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	// int k[] = { 1, 3, 2 };
+	// sort::bubble_sort(k, sizeof(k) / sizeof(int));
+	// file::write_text(k, sizeof(k) / sizeof(int), "result.txt");
+	return 0;
+}
