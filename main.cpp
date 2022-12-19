a875a6c390ffc4ab0538ef96f0943d0358a3da2e
tringuyen123 <95064327+tringuyen2711@users.noreply.github.com>
2022-12-19 14:00:04 +0700

    run to test function quick sort

--- main.cpp
+++ main.cpp
@@ -6,18 +6,25 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
 	int sample[] = { 1, 2, 3, 4, 5, 9, 8, 7 };
-	experiment1::run(sample, 8);
+	size_t n = sizeof(sample) / sizeof(int);
+	output::print(sample, 8);
+	sort::quick_sort(sample, n);
+	output::print(sample, 8);
+	/*experiment1::run(sample, 8);
 	if (argc == 2) {
-		int experiment = atoi(argv[1]);
+		int experiment = atoi(argv[2]);
 		switch (experiment) {
 		case 1:
 			experiment1::run(sample, 8);
 			break;
 		case 2:
+			output::print(sample, 8);
+			sort::quick_sort(sample, n);
+			output::print(sample, 8);
 			break;
 		default:
 			break;
 		}
-	}
+	}*/
 	return 0;
 }
